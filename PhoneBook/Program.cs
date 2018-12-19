992048a35a3197a0a25a199e272040f63f436ac1
hjh8916 <hjh8916@gmail.com>
2018-12-19 22:54:15 +0900

    Change Main Method
    메인 메소드 내용 수정.

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -7,6 +7,29 @@ using System.Threading.Tasks;
 namespace PhoneBook {
     class Program {
         static void Main(string[] args) {
+            UserInterface userInterface = new UserInterface();
+            Manager manager = new Manager();
+            while (true) {
+                switch (userInterface.menu()) {
+                    case 1:
+                        userInterface.add(manager);
+                        break;
+                    case 2:
+                        userInterface.find(manager);
+                        break;
+                    case 3:
+                        userInterface.change(manager);
+                        break;
+                    case 4:
+                        userInterface.remove(manager);
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        break;
+
+                } 
+            }
         }
     }
 }
