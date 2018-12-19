4c90a9ae320dec1f72e5ee0b6d45bcea48843ca3
hjh8916 <hjh8916@gmail.com>
2018-12-19 22:51:28 +0900

    add Profile C# script
    프로필 기본 정보를 저장하는 클래스이다.

---
+++ PhoneBook/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook {
+    class Profile {
+       public String name;
+       public String phoneNumber;
+
+        public Profile(String name, String phoneNumber) {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+        public override string ToString() {
+            return $"이름: { this.name}\n전화번호 : {this.phoneNumber}";  
+        }
+    }
+}
