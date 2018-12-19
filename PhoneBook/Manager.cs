bba7851794767b6bc2c8cabd886330d4a4153bc3
hjh8916 <hjh8916@gmail.com>
2018-12-19 22:49:23 +0900

    add Manager C# script
    매니저 파일은 데이터의 CRUD의 동작을 하는 메소드로 이루어져 있다.

---
+++ PhoneBook/Manager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook {
+    class Manager {
+        Dictionary<String, Profile> book = new Dictionary<String, Profile>();
+
+        public void save(Profile profile) {
+            book.Add(profile.name, profile);
+        }
+
+        public Profile find(String name) {
+            Profile profile;
+            book.TryGetValue(name, out profile);
+            return profile;
+        }
+
+        public void changeByPhoneNumber(String name, String phoneNumber) {
+            Profile profile = find(name);
+            profile.phoneNumber = phoneNumber;
+        }
+
+        public void remove(String name) {
+            book.Remove(name);
+        }
+    }
+}
